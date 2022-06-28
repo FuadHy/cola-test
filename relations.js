4fb2bdffb54a1630e18ccc6226eac4041d98db92
fuadhy <fuadmail3@gmail.com>
2022-06-28 06:03:00 +0300

    initial

--- relations.js
+++ relations.js
@@ -2,5 +2,5 @@ const Product = require('./models/productModel.js')
 const Purchase = require('./models/purchaseModel.js')
 
 module.exports = function(){
-    Product.belongsTo(Purchase, {foreignKey: 'product', onDelete: 'CASCADE', onUpdate: 'CASCADE'})
+    Purchase.belongsTo(Product, {foreignKey: 'product', onDelete: 'CASCADE', onUpdate: 'CASCADE'})
 }
